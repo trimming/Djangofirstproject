44e0453c56bb8cb87d8dd43adf5489639646b7bb
Alexey Golikov <lexgolikoff@gmail.com>
2024-06-30 17:52:13 +0300

    added task three from lesson_3

--- myfirstproject/myapp3/urls.py
+++ myfirstproject/myapp3/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home
-from .views import get_info_about
+from .views import home, get_info_about, flip_cube, flip_coin, get_random_number
+
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', get_info_about, name='about'),
+    path('game_coin/<int:count>', flip_coin, name='game_coin'),
+    path('game_cube/<int:count>', flip_cube, name='game_cube'),
+    path('game_number/<int:count>', get_random_number, name='game_coin'),
 ]
--- myfirstproject/myapp3/views.py
+++ myfirstproject/myapp3/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.shortcuts import render
 from django.http import HttpResponse
 import logging
@@ -17,3 +19,33 @@ def get_info_about(request):
     context = {'name': 'Алексей',
                'title': 'Обо мне'}
     return render(request, 'myapp3/about.html', context)
+
+
+def flip_coin(request, count: int):
+    logger.info("Coin page accessed")
+    res = [random.choice(["Орёл", "Решка"]) for _ in range(count)]
+    context = {
+        'title': 'Flip coin',
+        'content': res,
+    }
+    return render(request, 'myapp3/games.html', context)
+
+
+def flip_cube(request, count: int):
+    logger.info("Cube page accessed")
+    res = [random.randint(1, 6) for _ in range(count)]
+    context = {
+        'title': 'Random roll',
+        'content': res
+    }
+    return render(request, 'myapp3/games.html', context)
+
+
+def get_random_number(request, count: int):
+    logger.info("Number page accessed")
+    result = [random.randint(1, 100) for _ in range(count)]
+    context = {
+        'title': 'Random number',
+        'content': result
+    }
+    return render(request, 'myapp3/games.html', context)
