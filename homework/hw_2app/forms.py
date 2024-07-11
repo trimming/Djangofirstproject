16cb856fb91bb92d174af1ea649b9bd962c9a27a
trimming <55543686+trimming@users.noreply.github.com>
2024-07-11 11:52:56 +0200

    Update forms.py

--- homework/hw_2app/forms.py
+++ homework/hw_2app/forms.py
@@ -16,3 +16,11 @@ class ProductForm(forms.Form):
 
 class ImageForm(forms.Form):
     image = forms.ImageField()
+
+
+class ProductFormForAdd(forms.Form):
+    name = forms.CharField(min_length=3, max_length=60, label='Название товара')
+    desc = forms.CharField(widget=forms.Textarea(), label='Описание')
+    price = forms.DecimalField(max_digits=7, decimal_places=2, label='Цена')
+    quantity = forms.IntegerField(label='Количество')
+    add_date = forms.DateTimeField(initial=datetime.date.today, label='Дата изменения')
